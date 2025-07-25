94a7f58ae2f5ec0c56b3e61968918ae6e098c942
Neil <cao-x@smfl.co.jp>
2025-07-25 17:25:17 +0800

    rust: 闭包

--- _13/src/main.rs
+++ _13/src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
     println!("Hello, world!");
+    let x = test_fn(false).unwrap_or_else(|| 8);
+    println!("x: {}", x);
+
+    let mut v1 = vec![S{
+        x: 1,
+        y: 2,
+    },S{
+        x: 8,
+        y: 9,
+    },S{
+        x: 3,
+        y: 4,
+    }];
+    let mut p1 = || v1.push(S { x: 4, y: 10 });
+    // println!("v1: {v1:?}");
+    p1();
+    let mut v2: Vec<String> = Vec::new();
+    let str = String::from("hello");
+    println!("v1: {v1:?}");
+    // v1.sort_by_key(|i| i.x);
+    v1.sort_by_key(|i| {
+        v2.push(str.clone());
+        i.x
+    });
+    println!("v1: {v1:?}");
+    println!("v2: {v2:?}");
+}
+
+#[derive(Debug)]
+struct  S {
+    x: i8,
+    y: i8,
+}
+
+fn test_fn(x: bool) -> Option<i8>{
+    if x {
+        Some(1)
+    } else {
+        None
+    }
 }
