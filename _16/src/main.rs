931c725e72830ba21a885addf40d40e96da70b5d
Neil <cao-x@smfl.co.jp>
2025-09-01 17:22:13 +0800

    rust: 多线程, 异步

--- _16/src/main.rs
+++ _16/src/main.rs
@@ -1,3 +1,89 @@
+use std::{sync::{mpsc, Arc, Mutex}, thread};
+
+// fn main() {
+//     println!("Hello, world!");
+//     let handle = thread::spawn(|| {
+//         for i in 1..5 {
+//             println!("spawn {}", i);
+//         }
+//     });
+//     handle.join().unwrap();
+//     // thread::sleep(std::time::Duration::from_millis(1));
+//     for i in 1..5 {
+//         println!("main {}", i);
+//         // thread::sleep(std::time::Duration::from_millis(1));
+//     }
+//     // handle.join().unwrap();
+// }
+
+// fn main() {
+//     let v = vec![1, 2, 3];
+//     let handler = thread::spawn(move || {
+//         println!("spawn: {:?}", v);
+//     });
+//     // println!("main: {:?}", v);
+//     handler.join().unwrap();
+// }
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//     thread::spawn(move || {
+//         thread::sleep(std::time::Duration::from_millis(5000));
+//         let v = vec![1, 2, 3];
+//         tx.send(v).unwrap();
+//         println!("spawn: sent {:?}", v);
+//     });
+//     let received = rx.recv().unwrap();
+//     println!("main: {:?}", received);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    let s2 = tx.clone();
+    thread::spawn(move || {
+        let v = vec![1, 2, 3];
+        for vi in v {
+            tx.send(vi).unwrap();
+            thread::sleep(std::time::Duration::from_secs(2));
+        }
+    });
+    thread::spawn(move || {
+        let v = vec![5, 6, 7];
+        for vi in v {
+            s2.send(vi).unwrap();
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+    for rec in rx {
+        println!("main: {:?}", rec);
+    }
 }
+
+
+// fn main() {
+//     let m = Mutex::new(5);
+//     {
+//         let mut mm = m.lock().unwrap();
+//         *mm = 10;
+//     }
+//     println!("m = {:?}", m);
+// }
+
+
+// fn main() {
+//     let m = Arc::new(Mutex::new(5));
+//     let mut handlers = vec![];
+//     for i in 0..10 {
+//         let m = m.clone();
+//         let handler = thread::spawn(move || {
+//             let mut mm = m.lock().unwrap();
+//             *mm += i;
+//             println!("mm = {:?}", mm);
+//         });
+//         handlers.push(handler);
+//     }
+//     for handler in handlers {
+//         handler.join().unwrap();
+//     }
+//     println!("m = {:?}", m);
+// }
--- _17/src/main.rs
+++ _17/src/main.rs
@@ -1,3 +1,123 @@
+// use trpl::Either;
+
+// fn main() {
+//     trpl::run(async {
+//         // let title: String = load_title("https://app.slack.com/client/E06D01RSM29").await;
+//         // println!("Title: {}", title);
+//         let title: String = match trpl::race(load_title("https://app.slack.com/client/E06D01RSM29"), load_title("https://app.slack.com/client/E06D01RSM29")).await {
+//             Either::Left(left) => left,
+//             Either::Right(right) => right,
+//         };
+//         println!("Title: {}", title);
+//     });
+//     println!("Hello, world!");
+// }
+
+// async fn load_title(url: &str) -> String {
+//     let res = trpl::get(&url).await.text().await;
+//     trpl::Html::parse(&res).select_first("title").map(|i|i.inner_html()).unwrap()
+// }
+
+// extern crate trpl;
+
+// use std::time::Duration;
+
+// fn main() {
+//     trpl::run(async {
+//         let fut1 = async {
+//             for i in 1..10 {
+//                 println!("hi number {i} from the first task!");
+//                 trpl::sleep(Duration::from_millis(500)).await;
+//             }
+//         };
+//         // for i in 1..10 {
+//         //     println!("hi number {i} from the first task!");
+//         //     trpl::sleep(Duration::from_millis(500)).await;
+//         // }
+
+//         let fut2 = async {
+//             // for i in 1..5 {
+//             //     println!("hi number {i} from the second task!");
+//             //     trpl::sleep(Duration::from_millis(500)).await;
+//             // }
+//         };
+
+//         for i in 1..5 {
+//             println!("hi number {i} from the second task!");
+//             trpl::sleep(Duration::from_millis(500)).await;
+//         }
+
+//         fut1.await;
+
+//         // trpl::join(fut1, fut2).await;
+//     });
+// }
+
+// extern crate trpl; // required for mdbook test
+
+// use std::time::Duration;
+
+// fn main() {
+//     trpl::run(async {
+//         let (tx, mut rx) = trpl::channel();
+
+//         let vals = vec![
+//             String::from("hi"),
+//             String::from("from"),
+//             String::from("the"),
+//             String::from("future"),
+//         ];
+
+//         for val in vals {
+//             tx.send(val).unwrap();
+//             trpl::sleep(Duration::from_millis(500)).await;
+//         }
+
+//         while let Some(value) = rx.recv().await {
+//             println!("received '{value}'");
+//         }
+
+//         // while let Some(i) = rx.recv().await {
+//         //     println!("received '{i}'");
+//         // }
+//     });
+// }
+
+extern crate trpl; // required for mdbook test
+
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    trpl::run(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let txh= async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rxh =async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+            // for i in rx.recv().await {
+            //     println!("received '{i}'");
+            // }
+        };
+
+        trpl::join(txh, rxh).await;
+
+        // while let Some(i) = rx.recv().await {
+        //     println!("received '{i}'");
+        // }
+    });
 }
